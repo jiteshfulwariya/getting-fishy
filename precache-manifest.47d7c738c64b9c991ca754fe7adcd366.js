cc23d1aa1edd99ec41463736649a030e27de7592
Jitesh <jbf9109@rit.edu>
2020-09-28 11:33:31 -0400

    Initial Fish

---
+++ precache-manifest.47d7c738c64b9c991ca754fe7adcd366.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b416ab3c3f2be018010b519290440bf2",
+    "url": "/catch-of-the-day/index.html"
+  },
+  {
+    "revision": "591ef481c4353be611ed",
+    "url": "/catch-of-the-day/static/css/main.17d279e3.chunk.css"
+  },
+  {
+    "revision": "f23272349ec775e06480",
+    "url": "/catch-of-the-day/static/js/2.e1b6c027.chunk.js"
+  },
+  {
+    "revision": "a8c6aca52063a857962cec4d6f5ca45d",
+    "url": "/catch-of-the-day/static/js/2.e1b6c027.chunk.js.LICENSE"
+  },
+  {
+    "revision": "591ef481c4353be611ed",
+    "url": "/catch-of-the-day/static/js/main.75664d95.chunk.js"
+  },
+  {
+    "revision": "3207e2bc2fe412f8fbaf",
+    "url": "/catch-of-the-day/static/js/runtime-main.c82cac56.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/catch-of-the-day/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/catch-of-the-day/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/catch-of-the-day/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/catch-of-the-day/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/catch-of-the-day/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/catch-of-the-day/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/catch-of-the-day/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/catch-of-the-day/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/catch-of-the-day/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
